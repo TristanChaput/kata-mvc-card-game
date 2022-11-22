eed7416e630aee3232fbf5d53d403e31cbaaa09f
tristan <tristacha59@gmail.com>
2022-11-22 07:42:39 +0100

    test paul should have a card with face down

--- tests/test_players.py
+++ tests/test_players.py
@@ -53,8 +53,8 @@ def test_paul_should_be_registered(monkeypatch, game):
     monkeypatch.setattr("builtins.input", lambda _: "Paul")
     monkeypatch.setattr(Game, "MAX_PLAYERS_ALLOWED", 1)
     game.register_players()
-    players = game.get_players()
-    assert players[0].get_name() == "Paul"
+    player = game.get_a_player(index=0)
+    assert player.get_name() == "Paul"
 
 
 def test_should_return_players_when_a_game_with_players_is_created(
@@ -85,7 +85,7 @@ def test_paul_should_have_a_card(monkeypatch, game):
     monkeypatch.setattr(Game, "MAX_PLAYERS_ALLOWED", 1)
     game.register_players()
     game.give_a_card()
-    paul_hand = game.get_players()[0].get_hand()
+    paul_hand = game.get_a_player(index=0).get_hand()
     assert len(paul_hand) == 1
 
 
@@ -95,8 +95,17 @@ def test_paul_and_pierre_should_have_a_card_but_not_the_same(monkeypatch, game):
     monkeypatch.setattr(Game, "MAX_PLAYERS_ALLOWED", 2)
     game.register_players()
     game.give_a_card()
-    paul_hand = game.get_players()[0].get_hand()
-    pierre_hand = game.get_players()[1].get_hand()
+    paul_hand = game.get_a_player(index=0).get_hand()
+    pierre_hand = game.get_a_player(index=1).get_hand()
     assert len(paul_hand) == 1
     assert len(pierre_hand) == 1
     assert paul_hand != pierre_hand
+
+
+def test_paul_should_have_a_card_with_face_down(monkeypatch, game):
+    monkeypatch.setattr("builtins.input", lambda _: "Paul")
+    monkeypatch.setattr(Game, "MAX_PLAYERS_ALLOWED", 1)
+    game.register_players()
+    game.give_a_card()
+    paul_hand = game.get_a_player(index=0).get_hand()
+    assert paul_hand[0].is_turned_down()
