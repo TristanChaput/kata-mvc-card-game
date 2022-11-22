87e9d6ac3e8fdcfe6f983540b8f6930105e970b7
tristan <tristacha59@gmail.com>
2022-11-22 05:20:15 +0100

    create player hand logic

--- models/player.py
+++ models/player.py
@@ -1,9 +1,21 @@
+from typing import List
+from models.card import Card
+
+
 class Player:
 
     _name: str
+    _hand: List[Card]
 
     def __init__(self, name) -> None:
         self._name = name
+        self._hand = []
 
     def get_name(self):
         return self._name
+
+    def get_hand(self):
+        return self._hand
+
+    def add_a_card_in_hand(self, card):
+        self._hand.append(card)
